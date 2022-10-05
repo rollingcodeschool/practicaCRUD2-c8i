e44af68f65bea5ba3339799314e5e7d3ca18666c
Emilse Arias <emilsearias@gmail.com>
2022-10-05 18:44:39 -0300

    agregamos react-hook-form

--- src/components/views/producto/CrearProducto.jsx
+++ src/components/views/producto/CrearProducto.jsx
@@ -1,16 +1,32 @@
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Container } from "react-bootstrap";
+import {useForm} from 'react-hook-form';
+
+
 const CrearProducto = () => {
+
+    const {register, handleSubmit, formState:{errors}} = useForm();
+
+    const onSubmit = (data)=>{
+        console.log(data)
+    }
+
     return (
-        <>
+        <Container className="mainSection">
             <section className="container my-3">
                 <h3 className="display-4">Nuevo Producto</h3>
                 <hr />
             </section>
             <section className="container my-3">
-                <Form>
+                <Form noValidate onSubmit={handleSubmit(onSubmit)}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Nombre producto*</Form.Label>
-                        <Form.Control required type="text" placeholder="Ej:cafe" />
+                        <Form.Control required 
+                        type="text" 
+                        placeholder="Ej:cafe" 
+                        {...register('nombreProducto',{
+                            required:true
+                        } )}
+                         />
                         <Form.Text className="text-danger">algun error</Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -38,7 +54,7 @@ const CrearProducto = () => {
                     </Button>
                 </Form>
             </section>
-        </>
+        </Container>
     );
 };
 
